571c5b8de77b418c93a74b55de649eaa086a17f2
jcarlos <carlosmaster10@gmail.com>
2015-08-24 19:58:26 -0300

    adjust width and height of the window

--- desktop/src/br/com/killaliens/desktop/DesktopLauncher.java
+++ desktop/src/br/com/killaliens/desktop/DesktopLauncher.java
@@ -1,12 +1,26 @@
 package br.com.killaliens.desktop;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 import br.com.killaliens.KillAliens;
 
 public class DesktopLauncher {
+    private static final double ADJUST_HEIGHT = 0.10;
+    private static final double ADJUST_WIDTH = 0.02;
+    
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		config.height = (int) (screenSize.getHeight() 
+		        - (screenSize.getHeight()*ADJUST_HEIGHT));
+		config.width = (int) (screenSize.getWidth() 
+		        - (screenSize.getWidth()*ADJUST_WIDTH));
+		
 		new LwjglApplication(new KillAliens(), config);
 	}
 }
