fb53bf4b4c8bb61937b34b7fe014a94bb57b8539
jcarlos <carlosmaster10@gmail.com>
2015-08-24 12:41:38 -0300

    first commit

---
+++ android/src/br/com/killaliens/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package br.com.killaliens.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import br.com.killaliens.KillAliens;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new KillAliens(), config);
+	}
+}
---
+++ desktop/src/br/com/killaliens/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package br.com.killaliens.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.com.killaliens.KillAliens;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new KillAliens(), config);
+	}
+}
